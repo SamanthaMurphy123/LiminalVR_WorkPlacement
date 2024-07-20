1a5e936066d1b156cb41b5271921e206315e883c
Xavier <67317037+Thekoa@users.noreply.github.com>
2024-07-20 19:35:10 +1000

    Importing Refined bamboo and bird mesh

---
+++ Assets/Group2_Assets/3D Assets/test export/textures/AdjustPivot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustPivot : MonoBehaviour
+{
+    public Vector3 newPivotPosition;
+
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] -= newPivotPosition;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+
+        transform.position += newPivotPosition;
+    }
+}
